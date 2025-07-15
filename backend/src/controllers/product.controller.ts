700200814da5c29d2d7ecaf3ed78acf6ddb08071
DKMFzF <flim.win@yandex.ru>
2025-07-15 10:55:47 +0700

    format: product.controller

--- backend/src/controllers/product.controller.ts
+++ backend/src/controllers/product.controller.ts
@@ -1,31 +1,31 @@
 import { type NextFunction, type Request, type Response } from 'express';
 import { Error as MongooseError } from 'mongoose';
-
 import { ServerError, BedRequest, ObjectExists } from '../error';
 import Product from '../models/product';
 
 export const getAllProducts = async (
   _req: Request,
   res: Response,
-  next: NextFunction
-) =>
-  Product.find({})
-    .then((users) =>
-      res.send({
-        items: {
-          ...users,
-        },
-        total: users.length,
-      })
-    )
-    .catch((err) => next(new ServerError(err.message)));
+  next: NextFunction,
+) => Product.find({})
+  .then((users) => res.send({
+    items: { ...users },
+    total: users.length,
+  }))
+  .catch((err) => next(new ServerError(err.message)));
 
 export const createProduct = async (
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
-  const { title, image, category, description, price } = req.body;
+  const {
+    title,
+    image,
+    category,
+    description,
+    price,
+  } = req.body;
 
   return Product.create({
     title,
@@ -36,9 +36,12 @@ export const createProduct = async (
   })
     .then(() => res.send('Продукт создан'))
     .catch((err) => {
-      if (err instanceof Error && err.message.includes('E11000'))
+      if (err instanceof Error && err.message.includes('E11000')) {
         next(new ObjectExists('Such a product already exists'));
-      if (err instanceof MongooseError) next(new BedRequest('Incorrect data'));
+      }
+      if (err instanceof MongooseError) {
+        next(new BedRequest('Incorrect data'));
+      }
       return next(new ServerError(err.message));
     });
 };
